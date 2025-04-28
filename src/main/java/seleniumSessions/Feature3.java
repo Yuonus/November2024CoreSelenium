767c8c9343e99ca9e65ae28298faf1a694c0ee20
Yuonus <syuonus@gmail.com>
2025-04-28 08:17:25 -0400

    Added feature 3.

---
+++ src/main/java/seleniumSessions/Feature3.java
@@ -0,0 +1,11 @@
+package seleniumSessions;
+
+public class Feature3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is F3.");
+
+	}
+
+}
